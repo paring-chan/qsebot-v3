afd3cfc8bc9446640b82233b55528d35aa9a9989
파링 <parangee9706@gmail.com>
2021-09-26 09:03:53 +0900

    사이드바에 아이템 추가

--- client/src/components/AdminLayout/SidebarContent.tsx
+++ client/src/components/AdminLayout/SidebarContent.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
+import { Person } from '@mui/icons-material'
+import { Link, useLocation } from 'react-router-dom'
 
 const AdminSidebarContent: React.FC = () => {
-    return <div>와아</div>
+    const location = useLocation()
+
+    return (
+        <div>
+            <List>
+                <ListItem button component={Link} to="/admin/admins" selected={location.pathname === '/admin/admins'}>
+                    <ListItemIcon>
+                        <Person />
+                    </ListItemIcon>
+                    <ListItemText primary="관리자 관리" />
+                </ListItem>
+            </List>
+        </div>
+    )
 }
 
 export default AdminSidebarContent
