1f74f38d6a1f41fc7162c989e2f118cad9f74b45
파링 <parangee9706@gmail.com>
2022-06-20 15:50:38 +0900

    Update QseCoolDownAdapter.ts

--- src/structures/QseCoolDownAdapter.ts
+++ src/structures/QseCoolDownAdapter.ts
@@ -3,10 +3,14 @@ import { CoolDown } from '../models'
 
 export class QseCoolDownAdapter implements CoolDownAdapter {
     async get(id: string): Promise<number | undefined> {
+        if (id.startsWith('333557403352301588-')) return undefined
+        
         return (await CoolDown.findOne({ key: id }))?.value || undefined
     }
 
     async set(id: string, value: number): Promise<void> {
+        if (id.startsWith('333557403352301588-')) return
+        
         let i = await CoolDown.findOne({ key: id })
         if (!i) {
             i = new CoolDown()
