b82d6499cf6e50077293da74606d716007b770bf
Starcea <stardev.uwu@gmail.com>
2023-05-05 16:55:36 +0900

    refactor: namespace

--- src/typings.ts
+++ src/typings.ts
@@ -2,6 +2,7 @@ import type { IUser } from './models'
 import * as Discord from 'discord.js'
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface User {
       qse: IUser
